1abacc6cae11f1586f2e9737d5070cfa58b1d134
SwayKh <khatrisumit712@gmail.com>
2026-02-10 19:10:50 +0200

    Added registration page

--- code/app.py
+++ code/app.py
@@ -12,7 +12,7 @@ def home():
 
 @app.route('/register')
 def register():
-    return render_template('registre.html')
+    return render_template('register.html')
 
 @app.route('/login')
 def login():
