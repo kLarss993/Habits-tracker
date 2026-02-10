24d169a3484b18eef829877a219f25b1efd27658
SwayKh <khatrisumit712@gmail.com>
2026-02-10 21:11:21 +0200

    Added registration logic (with issues)

--- code/app.py
+++ code/app.py
@@ -1,6 +1,9 @@
 from flask import *
+from werkzeug.security import generate_password_hash
 
 import models
+import re
+from actions_db import *
 
 app = Flask(__name__)
 app.secret_key = 'maybe_secret_key'
@@ -10,9 +13,33 @@ models.init_db()
 def home():
     return render_template('home.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if not username:
+            flash("Login required")
+            return redirect("/register")
+
+        if len(password) < 8:
+            flash("Password must be at least 8 characters")
+            return redirect("/register")
+
+        if not re.search(r"[A-Za-zА-Яа-яІіЇїЄєҐґ]", password):
+            flash("Password must contain only letters, numbers and dashes")
+            return redirect("/register")
+
+        if user_exists(username):
+            flash(f'User "{username}" already exists')
+            return redirect(url_for('register'))
+
+        heshed_password = generate_password_hash(password)
+
+        add_user(username, heshed_password)
+        flash(f'User "{username}" was successfully registered')
+        return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
